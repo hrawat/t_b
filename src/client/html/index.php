86d86a7c1e2d01d8c87a018af6bb0f97757a50cc
hrawat <harish.s.rawat@gmail.com>
2012-08-18 16:21:10 -0700

    add support for auto provisioning users

---
+++ src/client/html/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: harish
+ * Date: 8/15/12
+ * Time: 9:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
--- src/server/services/auth/AuthenticationService.php
+++ src/server/services/auth/AuthenticationService.php
@@ -1,57 +1,53 @@
 <?php
 
 require_once (dirname(__FILE__) . "/../../lib/facebook-php-sdk/facebook.php");
+require_once(dirname(__FILE__) . "/../../services/user/UserService.php");
+require_once(dirname(__FILE__) . "/../../utils/Logger.php");
 
 class AuthenticationService {
 
     const UID_KEY = "uid";
 
+    private static $faceBookInstance;
+
+    private static function getFaceBookInstance() {
+        if (self::$faceBookInstance == NULL) {
+            self::$faceBookInstance = new Facebook(array(
+                'appId'  => '420637114648782',
+                'secret' => '202d56805a3f75acd1a8f2e0e941acfb',
+            ));
+        }
+        return self::$faceBookInstance;
+    }
+
     /**
      * @static
      * Return logged in user id. Identify logged in user thru two mechanisms - our session or
-     * thru FB
+     * thru FB and do auto provisioning
      */
     public static function getLoggedInUserId() {
         $uid = self::getUidFromSession();
         if ($uid == NULL) {
             // Get the session from FB
-            $facebook = new Facebook(array(
-                'appId'  => '420637114648782',
-                'secret' => '202d56805a3f75acd1a8f2e0e941acfb',
-            ));
-
+            $faceBook = self::getFaceBookInstance();
             // See if there is a user from a cookie
-            $fbUid = $facebook->getUser();
+            $fbUid = $faceBook->getUser();
             if ($fbUid == 0) {
                 error_log("did not get user from signed cookie");
                 return NULL;
             } else {
-                $user = UserService::lookupUserByFaceBookUid($fbUid);
-                if ($user == NULL) {
-                    $userProfile = $facebook->api("/me");
-                    $uid = UserService::createUser();
-                } else {
-                    $uid = $user['id'];
+                $uid = self::lookupOrCreateFBUser($fbUid);
+                if (isset($uid)) {
+                    self::setUidInSession($uid);
                 }
-
-
-
             }
-        }
-    }
-
-
-    public static function isValidSession() {
-        session_start();
-        $uid = self::getUidFromSession();
-        if (isset($uid)) {
-            return TRUE;
         } else {
-            return FALSE;
+            return $uid;
         }
     }
 
-    public static function getUidFromSession() {
+
+    private static function getUidFromSession() {
         $uid = NULL;
         if (isset($_SESSION[self::UID_KEY])) {
             $uid = $_SESSION[self::UID_KEY];
@@ -59,10 +55,29 @@ class AuthenticationService {
         return $uid;
     }
 
-    public static function setUidInSession($uid) {
+    private static function setUidInSession($uid) {
         session_start();
         $_SESSION[self::UID_KEY] = $uid;
 
+    }
+
+    private static function lookupOrCreateFBUser($fbUid) {
+        $user = UserService::lookupUserByFaceBookUid($fbUid);
+        if ($user == NULL) {
+            $faceBook = self::getFaceBookInstance();
+            $userProfile = $faceBook->api("/me");
+            if (isset($userProfile)) {
+                $uid = UserService::createFBUser($fbUid, $userProfile['firstName'],
+                                                        $userProfile['lastName'], $userProfile['gender'],
+                                                        $userProfile['email'], NULL);
+                return $uid;
+            } else {
+                Logger::error("Couldn't get details of $fbUid");
+                return NULL;
+            }
+        } else {
+            return $user['id'];
+        }
     }
 
 }

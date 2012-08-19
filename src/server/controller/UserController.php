d40a1ca213bd6b6566a729bc709b46986904db81
hrawat <harish.s.rawat@gmail.com>
2012-08-18 17:02:08 -0700

    first cut user controller

---
+++ src/server/controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once (dirname(__FILE__) . "../services/auth/AuthenticationService.php");
+require_once (dirname(__FILE__) . "../utils/ErrorCodes.php");
+
+function getLoggedInUserDetails() {
+    $user = AuthenticationService::getLoggedInUserId();
+    $retValue = array();
+    if (isset($retValue)) {
+        $retValue['success'] = TRUE;
+        $retValue['payload'] = $user;
+    } else {
+        $retValue['success'] = FALSE;
+        $retValue['errCode'] = ErrorCodes::USER_NOT_AUTHENTICATED;
+        $retValue['errMsg'] =  ErrorCodes::USER_NOT_AUTHENTICATED_MSG;
+    }
+    $retValueInJson = json_encode($retValue);
+    echo $retValueInJson;
+}
+
+getLoggedInUserDetails();
+
+?>
---
+++ src/server/utils/ErrorCodes.php
@@ -0,0 +1,7 @@
+<?php
+
+class ErrorCodes {
+    const USER_NOT_AUTHENTICATED = 1;
+    const USER_NOT_AUTHENTICATED_MSG = "User is not authenticated";
+
+}

5f49d913463f9e15bdac92e0adb48e9aabcfcbe1
hrawat <harish.s.rawat@gmail.com>
2012-09-04 21:23:17 -0700

    checkin raw files to start integration with annie's page

--- src/client/html/js/model/UserModel.js
+++ src/client/html/js/model/UserModel.js
@@ -2,86 +2,24 @@ if (typeof(TaskBoard) == "undefined") {
     TaskBoard = {};
 }
 
-TaskBoard.UserModel = function() {
-
-    var fbUserUid, userLoggedIn, retValue, initialized =false;
-    var initCallBacks = new Array();
-
-    // Implement singleton pattern : check if an instance already exists
-    if (TaskBoard.UserModel.singletonInstance != undefined) {
-        return TaskBoard.UserModel.singletonInstance;
-    }
-
-    function _initialize() {
-        if ($("#fb-root").length == 0) {
-            $("body").prepend("<div id='fb-root' />");
-        }
-
-        // Initialize SDK
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId      : '420637114648782', // App ID
-                channelUrl : 'http://ec2-107-20-25-51.compute-1.amazonaws.com/channel.html', // Channel File
-                status     : true, // check login status
-                cookie     : true, // enable cookies to allow the server to access the session
-                xfbml      : true  // parse XFBML
-            });
-
-            FB.getLoginStatus( function(response) {
-                TaskBoard.UserModel._fbAPIInitialized = true;
-                TaskBoard.UserModel._fbAPIInitializing = false;
-                if (response.status == 'connected') {
-                    fbUserUid = response.authResponse.userID;
-                    userLoggedIn = true;
-                } else {
-                    userLoggedIn = false;
-                }
-                for(var index=0; index < initCallBacks.length; index++) {
-                   initCallBacks[index].apply(retValue);
-                }
-                initialized = true;
-            });
-
-
-        };
-
-        // Load the SDK Asynchronously
-        (function(d){
-            var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement('script'); js.id = id; js.async = true;
-            js.src = "http://connect.facebook.net/en_US/all.js";
-            ref.parentNode.insertBefore(js, ref);
-        }(document));
-
-    }
-
-    _initialize();
-
-
-    retValue = new (function() {
-
-        this.addInitializationCallBack = function(callBackFunction) {
-            if (initialized) {
-                callBackFunction.apply(this);
+TaskBoard.UserModel = {
+    getUserInfo :function(successCallback, failureCallback) {
+        var userInfoReq = $.ajax({
+            url : "userInfo",
+            dataType : "json",
+            type : "GET"
+        });
+        userTasksReq.done(function(result) {
+            if (result['success']) {
+                successCallBack.call(this, result['payload']);
             } else {
-                initCallBacks.push(callBackFunction);
+                failureCallBack.call(this, result['errCode'], result['errMsg']);
             }
-        }
 
-        this.getUserId = function() {
-            return fbUserUid;
-        }
+        });
 
-        this.isUserLoggedIn = function() {
-            return userLoggedIn;
-        }
+    }
 
-    });
-
-    TaskBoard.UserModel.singletonInstance = retValue;
-    return retValue;
-
-};
+}
 
 
---
+++ src/client/html/js/view/Header.js
@@ -0,0 +1,20 @@
+/**
+ * This view is responsible for setting the header
+ */
+
+if (typeof(TaskBoard) == "undefined") {
+    TaskBoard = {};
+    TaskBoard.view = {};
+}
+
+TaskBoard.view.Header = {
+    render : function(data) {
+        $("#existingTasksCountId").text(data.existingTasksCount);
+        $("#tasksDoneInWeekCountId").text(data.tasksDoneInWeekCount);
+        $("#userSmallAvatarUrlId").attr("src", data.userSmallAvatarUrl)
+    }
+}
+
+
+
+

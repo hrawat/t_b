b6cdff30f8c1c5c16011139b532e3d367b52e328
hrawat <harish.s.rawat@gmail.com>
2012-09-27 19:29:18 -0700

    authentocation controller

---
+++ src/server/controller/AuthenticationController.php
@@ -0,0 +1,40 @@
+<?php
+
+function handleRequest() {
+   if (!isset($_GET['reqType'])) {
+        $retValue = ControllerUtils::getErrorResponse(ErrorCodes::INVALID_ARGS, "Mandatory parameter reqType missing");
+    } else {
+        $retValue = handleRequestInternal();
+    }
+    $retValueInJson = json_encode($retValue);
+    Logger::debug("taskService", "return value $retValueInJson");
+    echo $retValueInJson;
+}
+
+
+function handleRequestInternal() {
+    try {
+        $reqType = $_GET['reqType'];
+        if ($reqType == 'logout') {
+            $retValue = handleLogout();
+            return $retValue;
+        } else {
+            $retValue = ControllerUtils::getErrorResponse(ErrorCodes::INVALID_REQUEST, "Request $reqType is not supported");
+            return $retValue;
+        }
+    } catch (Exception $ex) {
+        $retValue = ControllerUtils::getErrorResponse(ErrorCodes::INTERNAL_ERROR, $ex->getMessage());
+        return $retValue;
+    }
+
+}
+
+function handleLogout() {
+    AuthenticationService::logout();
+    return ControllerUtils::getSuccessResponse(TRUE);
+}
+
+handleRequest();
+
+
+?>
